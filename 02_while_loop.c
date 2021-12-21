4b3e10269317e4d513a0da4488dd3af723d786dc
angad <angadreact01@gmail.com>
2021-12-21 10:21:47 -0700

    first commit

---
+++ 02_while_loop.c
@@ -0,0 +1,18 @@
+// WHILE LOOP
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i <= 20)
+    {
+        if (i >= 10)
+        {
+            printf(" Value of i is %d\n", i);
+        }
+             i++;
+        
+    }
+
+    return 0;
+}
